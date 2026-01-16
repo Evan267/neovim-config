aa869bbf84262e619bfb5b2123e523128c64b310
Evan267 <evan.berger@outlook.com>
2026-01-16 07:04:20 +0100

    Nouvelle key pour rename automatiquement

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -6,3 +6,13 @@ vim.keymap.set('n', 'K', vim.lsp.buf.hover, { desc = "Afficher la documentation"
 vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, { desc = "Renommer partout" })
 
 vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, { desc = "Actions de code" })
+
+vim.keymap.set("n", "<leader>cr", function()
+  local current_name = vim.fn.expand("<cword>")
+  vim.lsp.buf.rename()
+  
+  vim.defer_fn(function()
+    vim.cmd("wa")
+    vim.notify("Renommage de '" .. current_name .. "' terminé (Code + Fichier)", "info")
+  end, 500)
+end, { desc = "Rename Class and Sync File" })
